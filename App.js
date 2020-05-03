149f0e613fadb301daae159063967e379e94b02e
tataqt <tata_98@ukr.net>
2020-05-03 15:06:12 +0300

    добавления todo

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from './src/Navbar'
+import { AddTodo } from './src/AddTodo'
+import { Todo } from './src/Todo';
 
 export default function App() {
+  const [todos, setTodods] = useState([]);
+
+  const addTodo = (title) => {
+    setTodods(prev => [...prev, {
+      id: Date.now().toString(),
+      title
+    }
+    ]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Navbar title="Todo App" />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <View>
+          {todos.map(todo => <Todo todo={todo} key={todo.id} />)}
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
 });
---
+++ src/AddTodo.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Alert } from 'react-native';
+
+export const AddTodo = ({ onSubmit }) => {
+    const [value, setValue] = useState('');
+
+    const presshandler = () => {
+        if (value.trim()) {
+            onSubmit(value);
+            setValue('');
+        } else {
+            Alert.alert('Название дела не может быть пустым')
+        }
+    }
+
+    return (
+        <View style={styles.block}>
+            <TextInput
+                style={styles.input}
+                onChangeText={text => setValue(text)} // or {setValue}
+                value={value}
+                placeholder="Введите название дела..."
+                autoCorrect={false}
+                autoCapitalize="none"
+                //keyboardType="web-search"
+            />
+            <Button title="Добавить" onPress={presshandler} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    block: {
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 15
+    },
+    input: {
+        width: '70%',
+        padding: 10,
+        borderStyle: 'solid',
+        borderBottomWidth: 2,
+        borderBottomColor: '#3949ab'
+    }
+})
